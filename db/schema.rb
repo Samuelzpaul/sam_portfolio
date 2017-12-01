7385629eead1caa6cb5601f89210c4bb310feaa6
Samuel Paul <samuelpaul@Samuels-MBP.ad.uvu.edu>
2017-12-01 11:28:21 -0700

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171130222512) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
+    t.string "subtitle"
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
