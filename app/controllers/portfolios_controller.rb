0bbf5c9235703bb98d7eb13620fa06fa8b9964c5
Samuel Paul <samuelpaul@samuels-mbp.ad.uvu.edu>
2017-12-05 14:02:09 -0700

    Implemented image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,11 +8,11 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio_items.save
-        format.html { redirect_to portfolio_path, notice: 'Your portfolio has been submitted.' }
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio has been submitted.' }
       else
         format.html { render :new }
       end
