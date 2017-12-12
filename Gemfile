dd333d89a207f634e6fef30eaecf0b607d570e6c
Samuel Paul <samuelpaul@Samuels-MBP.ad.uvu.edu>
2017-12-12 10:39:07 -0700

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'devise', '~> 4.3'
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
+gem 'paul_view_tool', git: 'https://github.com/Samuelzpaul/paul_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    PaulViewTool::Renderer.copyright "Sam Paul", "All rights reserved"
+  end
 end
